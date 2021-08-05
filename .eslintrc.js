3a438aaed5fbe8bc8f69cb2d071fb3d49793f933
wangjp <wangjp1@fosun.com>
2021-08-05 19:56:50 +0800

    project init finish

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    //此项是用来告诉eslint找当前配置文件不能往父级查找
+    root: true,
+    // 此项指定环境的全局变量，下面的配置指定为浏览器环境
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        //此项是用来指定eslint解析器的，解析器必须符合规则，babel-eslint解析器是对babel解析器的包装使其与ESLint解析
+        parser: 'babel-eslint'
+    },
+    /*
+    下面这些rules是用来设置从插件来的规范代码的规则，使用必须去掉前缀eslint-plugin-
+    主要有如下的设置规则，可以设置字符串也可以设置数字，两者效果一致
+    "off" -> 0 关闭规则
+    "warn" -> 1 开启警告规则
+    "error" -> 2 开启错误规则
+  */
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 }
